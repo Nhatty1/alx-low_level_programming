67a03530ec58f0d3e49240683b8d67f8d4041324
Nhatty1 <teyoo143@gmail.com>
2022-07-06 14:29:16 -0700

    new letter

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *  print_alphabet - prints the English alphabet from a to z
+ *   Return: Nothing
+ *    /
+ *    void print_alphabet(void)
+ *    {
+ *    char c = 'a';
+ *    while (c <= 'z')
+ *    {
+ *    _putchar(c);
+ *    c++;
+ *    }
+ *    _putchar('\n');
+ *    }
