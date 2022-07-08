f8018d573ff09799b7e51ea7245bcd1a3e38bc78
Nhatty1 <teyoo143@gmail.com>
2022-07-08 07:53:18 -0700

    fixed

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * mul - Do something
- * Return: Return something
- * @a: do something
- * @b: do something
+ *mul - Do something
+ *Return: Return something
+ *@a: do something
+ *@b: do something
  */
 int mul(int a, int b)
 {
-	int multi;
-	multi = a * b;
-	return (multi);
+int multi;
+multi = a * b;
+return (multi);
 }
