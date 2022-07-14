b37a3f2638590fa313c3e127ce05fd0e8ae15b00
Nhatty1 <teyoo143@gmail.com>
2022-07-14 02:51:55 -0700

    stricting fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,115 +1,20 @@
 #include "main.h"
-
-#include <stdio.h>
-
 /**
- * print_line - prints a s bytes of a buffer
- *
- * @c: buffer to print
- *
- * @s: bytes of buffer to print
- *
- * @l: line of buffer to print
- *
- *
- *
- * Return: void
- *
+ * _strcat - appends src to the dest string
+ * @dest: string to append by src
+ * @src: string to append to dest
+ * Return: address of dest
  */
-
-void print_line(char *c, int s, int l)
-
+char *_strcat(char *dest, char *src)
 {
-
-int j, k;
-
-for (j = 0; j <= 9; j++)
-
+int i, j;
+i = j = 0;
+while (*(dest + i))
+i++;
+while ((*(dest + i) = *(src + j)))
 {
-
-if (j <= s)
-
-printf("%02x", c[l * 10 + j]);
-
-else
-
-printf("  ");
-
-if (j % 2)
-
-putchar(' ');
-
+i++;
+j++;
 }
-
-for (k = 0; k <= s; k++)
-
-{
-
-if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
-
-putchar(c[l * 10 + k]);
-
-else
-
-putchar('.');
-
-}
-
-}
-
-
-
-
-/**
- * print_buffer - prints a buffer
- *
- * @b: buffer to print
- *
- * @size: size of buffer
- *
- *
- *
- * Return: void
- *
- */
-
-void print_buffer(char *b, int size)
-
-{
-
-int i;
-
-
-
-
-for (i = 0; i <= (size - 1) / 10 && size; i++)
-
-{
-
-printf("%08x: ", i * 10);
-
-if (i < size / 10)
-
-{
-
-print_line(b, 9, i);
-
-}
-
-else
-
-{
-
-print_line(b, size % 10 - 1, i);
-
-}
-
-putchar('\n');
-
-}
-
-if (size == 0)
-
-putchar('\n');
-
+return (dest);
 }
