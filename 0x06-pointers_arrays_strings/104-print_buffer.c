e9fd67397edd89fb950b56a33eab8ea48ab96e62
Nhatty1 <teyoo143@gmail.com>
2022-07-13 17:38:08 -0700

    buffer fix

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -29,22 +29,14 @@ printf("  ");
 
 else
 printf("%02x", *(b + index + byte));
-
 if ((index % 2) != 0 && index != 0)
-
 printf(" ");
-
 }
-
 for (index = 0; index < 10; index++)
 {
-
 if ((index + byte) >= size)
-
 break;
-
 else if (*(b + index + byte) >= 31 &&
-
 *(b + index + byte) <= 126)
 printf("%c", *(b + index + byte));
 else
@@ -55,5 +47,5 @@ continue;
 printf("\n");
 }
 if (size <= 0)
-	printf("\n");
+printf("\n");
 }
