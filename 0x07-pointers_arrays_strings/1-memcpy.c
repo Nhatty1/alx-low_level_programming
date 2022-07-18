a6f3a8c42e2f27033a0ff39879cedbf47a242280
Nhatty1 <teyoo143@gmail.com>
2022-07-18 06:38:53 -0700

    memcpy hot fix

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,17 @@
 #include "main.h"
 
 /**
-* _strchr -> string character
-* @s: string given
-* @c: another char
-* Return: a string
+* _memcpy -> memory copy
+* @dest: is destination memory
+* @src: is source memory
+* @n: number of bytes to be copied
+* Return: string copied from source
 */
-char *_strchr(char *s,  char c)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
+unsigned int a;
+
+for (a = 0; a < n; a++)
+dest[a] = src[a];
+return (dest);
 }
